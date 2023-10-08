fa9c8cfd03c25a6978d44bf0814655f6190a4ede
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-08 15:54:09 -0400

    actual change...

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function(config, size)
     config.font_size = size
-    config.hide_tab_bar_if_only_one_tab = true
+    config.hide_tab_bar_if_only_one_tab = false
 end
 
 return M
