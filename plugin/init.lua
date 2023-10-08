a81ee7b0190ec1e189c2c3f117472d840e2130f3
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-08 16:00:32 -0400

    addtl stuff

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.setup = function(config, size)
+M.setup = function(config, size, hide)
     config.font_size = size
-    config.hide_tab_bar_if_only_one_tab = false
+    config.hide_tab_bar_if_only_one_tab = hide
 end
 
 return M
